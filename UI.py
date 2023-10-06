551b87494a260a203cd64c5a2bd02d0bc2a00b6b
Daniil <shishovdanil@gmail.com>
2023-10-06 13:47:52 +0300

    Протестированы пользовательские значения (закомментированы)

--- UI.py
+++ UI.py
@@ -1,9 +1,27 @@
 from jssp import *
+from pyomo.common.timing import report_timing, TicTocTimer
 
-
+timer = TicTocTimer()
+timer.tic('start')
+report_timing()
+print('Building model')
+print('--------------')
 model11 = build_model(1,1)
-solve_model(model11, True)
+timer.toc('Built model')
+solve_model(model11)
+# saved_solution = {}
+# for v in model11.component_objects(Var):
+#     saved_solution[v.name] = v
+
+# for v_name, value in saved_solution.items():
+#     getattr(model11, v_name).value = value  
+# model11.worker_assigned[25,8].set_value(1)
+# model11.worker_assigned[25,8].fix()  
+# model11.worker_assigned[35,16].set_value(1)
+# model11.worker_assigned[35,16].fix() 
+# solve_model(model11)
+timer.toc('Solved')
 plot_schedule(model11)
-solution_to_file(model11)
+# solution_to_file(model11)
 plot_worker_utilization(model11)
 # do_n_solutions(5, model11)
