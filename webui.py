0061c24c89daecfae872f120d268558f2ef1019c
plaguedoctor39 <shishovdanil@gmail.com>
2023-10-24 19:15:25 +0300

    добавлены интерактивные графики

--- webui.py
+++ webui.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from jssp import build_model, solve_model, generate_output, plot_schedule, plot_worker_utilization
+from jssp import build_model, plot_gantt_schedule, plot_worker_utilization_interactive, solve_model, generate_output, plot_schedule, plot_worker_utilization
 import io
 import base64
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -14,12 +14,13 @@ def index():
         param1 = int(request.form.get('param1'))
         param2 = int(request.form.get('param2'))
         param3 = int(request.form.get('param3'))
+        param4 = int(request.form.get('param4'))
         hard_deadline = bool(request.form.get('hard_deadline'))
         
         # Создаем и решаем модель
-        model = build_model(param1, param2, param3, hard_deadline=hard_deadline)
-        model.start_time[120].set_value(60)
-        model.start_time[120].fix()  
+        model = build_model(param1, param2, param3, param4, hard_deadline=hard_deadline)
+        # model.start_time[120].set_value(60)
+        # model.start_time[120].fix()  
         result = solve_model(model)
         
         if not result:
@@ -30,10 +31,12 @@ def index():
         output_data = generate_output(model)  
 
         # Генерируем и сохраняем графики
-        plot_schedule(model, show=False)
-        plot_worker_utilization(model, show=False)
+        # plot_schedule(model, show=False, mode='workers')
+        schedule_fig = plot_gantt_schedule(model, show=False, mode='workers')
+        worker_util_fig = plot_worker_utilization_interactive(model, show=False)
+        # plot_worker_utilization(model, show=False)
 
-        return render_template('results.html', projects=output_data['projects'], workers=output_data['workers'], additional_info=output_data['additional_info'])
+        return render_template('results.html', projects=output_data['projects'], workers=output_data['workers'], additional_info=output_data['additional_info'], schedule_fig=schedule_fig, worker_util_fig=worker_util_fig)
 
     return render_template('index.html')
 
